a4d685ebc1ee4ae955101bafa42c6dbb46ffbb0c
Noubissie Pavel <noubissiepavel@gmail.com>
2024-10-23 03:03:57 +0100

    eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,96 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+          allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+          allowHigherOrderFunctions: false,
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@angular-eslint/use-lifecycle-interface': 'error',
+      '@angular-eslint/no-output-on-prefix': 'error',
+      '@angular-eslint/use-component-view-encapsulation': 'error',
+      '@angular-eslint/component-class-suffix': 'error',
+      '@angular-eslint/directive-class-suffix': 'error',
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'no-irregular-whitespace': 'error',
+      'no-underscore-dangle': 'error',
+      'no-unused-vars': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-template': 'error',
+      'semi': 'error',
+      'space-before-function-paren': 'error',
+      'max-len': ['error', 120],
+      'curly': ['error', 'all'],
+      'eqeqeq': 'error',
+      'no-eval': 'error',
+      'no-extend-native': 'error',
+      'no-fallthrough': 'error',
+      'no-invalid-this': 'error',
+      'no-iterator': 'error',
+      'no-labels': 'error',
+      'no-lone-blocks': 'error',
+      'no-loop-func': 'error',
+      'no-multi-spaces': 'error',
+      'no-new': 'error',
+      'no-new-func': 'error',
+      'no-new-wrappers': 'error',
+      'no-shadow': 'error',
+      'no-use-before-define': 'error',
+      'no-useless-concat': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'prefer-arrow-callback': 'error',
+      'prefer-numeric-literals': 'error',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        {
+          accessibility: 'explicit',
+        },
+      ],
+    },
+
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+);
