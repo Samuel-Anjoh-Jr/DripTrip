835cb9d82b93c76ceab2c20053e876e2cbdaa8af
Noubissie Pavel Le  Roi <noubissiepavel@gmail.com>
2024-12-19 22:49:56 +0100

    feat(structure): file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/features.routes').then(m => m.routes),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then(m => m.routes),
+  },
+];
